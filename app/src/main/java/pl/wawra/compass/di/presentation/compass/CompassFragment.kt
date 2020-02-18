8e82c45f4c7db71ec66f7c7ccd7ce33aa21304ee
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2020-02-18 19:20:30 +0100

    compass rotation

--- app/src/main/java/pl/wawra/compass/di/presentation/compass/CompassFragment.kt
+++ app/src/main/java/pl/wawra/compass/di/presentation/compass/CompassFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation.RELATIVE_TO_SELF
+import android.view.animation.RotateAnimation
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_compass.*
 import pl.wawra.compass.R
 
 class CompassFragment : Fragment() {
@@ -17,9 +21,44 @@ class CompassFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        @Suppress("DEPRECATION")
-        viewModel = ViewModelProviders.of(this).get(CompassViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(CompassViewModel::class.java)
         return inflater.inflate(R.layout.fragment_compass, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupObservers()
+        setupClickListeners()
+        viewModel.updateDegree()
+    }
+
+    private fun setupObservers() {
+        viewModel.degreeChange.observe(
+            viewLifecycleOwner,
+            Observer { rotateImage(it.first, it.second) }
+        )
+    }
+
+    private fun setupClickListeners() {
+        fragment_compass_compass_image.setOnClickListener {
+            viewModel.updateDegree((System.currentTimeMillis() % 360).toDouble())
+        }
+    }
+
+    private fun rotateImage(previousDegree: Float, degree: Float) {
+        fragment_compass_compass_image.startAnimation(
+            RotateAnimation(
+                previousDegree,
+                degree,
+                RELATIVE_TO_SELF,
+                0.5f,
+                RELATIVE_TO_SELF,
+                0.5f
+            ).apply {
+                duration = 2000
+                fillAfter = true
+            }
+        )
     }
 
 }
--- app/src/main/java/pl/wawra/compass/di/presentation/compass/CompassViewModel.kt
+++ app/src/main/java/pl/wawra/compass/di/presentation/compass/CompassViewModel.kt
@@ -1,5 +1,6 @@
 package pl.wawra.compass.di.presentation.compass
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import pl.wawra.compass.database.daos.LocationDao
 import javax.inject.Inject
@@ -8,5 +9,18 @@ class CompassViewModel : ViewModel() {
 
     @Inject
     lateinit var locationDao: LocationDao
+
+    private var lastDegree: Float = 0.0F
+    val degreeChange = MutableLiveData<Pair<Float, Float>>()
+
+    fun updateDegree(degree: Double? = null) {
+        if (degree == null) {
+            degreeChange.postValue(Pair(0.0F, lastDegree))
+        } else {
+            val calculatedDegree = (Math.toDegrees(degree) + 360).toFloat() % 360
+            degreeChange.postValue(Pair(lastDegree, calculatedDegree))
+            lastDegree = calculatedDegree
+        }
+    }
 
 }
